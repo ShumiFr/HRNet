acd98a427ce8d1ea944dcdd7e638f6f1f21ceb39
Gagnaire Pauline <gagnaire.pauline@gmail.com>
2025-02-10 12:48:44 +0100

    Finalisation du projet

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["prop-types"],
+  },
+  resolve: {
+    alias: {
+      "prop-types": "prop-types/prop-types.js",
+    },
+  },
+});
